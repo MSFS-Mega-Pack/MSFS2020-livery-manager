dc7450dd1b9eb764377cf1acbaaab9e09b2de4b5
David Wheatley <hi@davwheat.dev>
2020-08-23 17:46:56 +0100

    Add extra validation

--- src/helpers/MSFS/ValidateFSDirectory.js
+++ src/helpers/MSFS/ValidateFSDirectory.js
@@ -9,10 +9,12 @@ import Path from 'path';
  * @return {Array} [0]: is path valid, [1]: error message
  */
 export default function ValidateFSDirectory(path) {
+  if (path.trim() === '') return [false, 'Please choose a directory'];
+  
   let p = path.endsWith('/') || path.endsWith('\\') ? path.slice(0, -1) : path;
 
   if (p.endsWith('Community') || p.endsWith('Official')) {
-    return [false, 'Did you choose one of the folders inside your installation directory? Choose the top one instead.'];
+    return [false, 'Did you choose one of the folders inside your installation directory? Choose the folder with the folders "Community" and "Official" inside.'];
   } else if (!fs.existsSync(Path.join(path, '\\Community'))) {
     return [false, 'The chosen folder seems to be missing one or more subdirectories. Did you select the right folder?'];
   } else {
