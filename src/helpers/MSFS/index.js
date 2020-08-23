b44365cd79cbd402a98b64899ac5c62f028694a6
David Wheatley <hi@davwheat.dev>
2020-08-23 19:49:57 +0100

    Export CFG parser from MSFS

--- src/helpers/MSFS/index.js
+++ src/helpers/MSFS/index.js
@@ -1,2 +1,3 @@
 export { default as GetPackagesDirectory } from './GetPackagesDirectory';
 export { default as ValidateFSDirectory } from './ValidateFSDirectory';
+export { default as ParseCFGFile } from './parseCfgFile';
