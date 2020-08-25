c9bae9c6d670d22c266f0227f075aa45117460c3
David Wheatley <hi@davwheat.dev>
2020-08-25 02:34:26 +0100

    Freeze exported routes object
    We shouldn't ever have a reason to change this on the fly. If we are, we're doing something wrong.

--- src/data/Routes.js
+++ src/data/Routes.js
@@ -4,8 +4,8 @@ import Setup from '../pages/Setup';
 import LiveryManager from '../pages/LiveryManager';
 import SplashScreen from '../pages/SplashScreen';
 
-export default [
+export default Object.freeze([
   { path: '/setup', component: <Setup /> },
   { path: '/manager', component: <LiveryManager /> },
   { path: '/', component: <SplashScreen /> },
-];
+]);
