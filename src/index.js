d17c17dec8791d33335b72a1da4d87fab8d34110
David Wheatley <hi@davwheat.dev>
2020-08-25 20:55:13 +0100

    Add developer debugging tools

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { render } from 'react-dom';
 import App from './components/App';
 
 import '@babel/polyfill';
+import Navigate from './helpers/Navigate';
+import Routes from './data/Routes';
 
 // Since we are using HtmlWebpackPlugin WITHOUT a template, we should create our own root node in the body element before rendering into it
 let root = document.createElement('div');
@@ -12,3 +14,11 @@ document.body.appendChild(root);
 
 // Now we can render our application into it
 render(<App />, document.getElementById('root'));
+
+// if (process.)
+InitialiseDeveloperFunctions();
+
+function InitialiseDeveloperFunctions() {
+  window.__navigate = Navigate;
+  window.__routes = Routes;
+}
