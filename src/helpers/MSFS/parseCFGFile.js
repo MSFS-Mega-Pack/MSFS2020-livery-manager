49b4bf13297e2dfda6e6e9fc76c07f17fbcf2491
Jaap <jaap@mrproper.dev>
2020-08-26 19:52:44 +0200

    Fix if stuff

--- src/helpers/MSFS/parseCFGFile.js
+++ src/helpers/MSFS/parseCFGFile.js
@@ -101,47 +101,37 @@ export function doesPlaneNumberExist(planeNumber) {
  */
 export function checkAirplaneObject(airplaneObject) {
   //Check and set values when needed
-  if (airplaneObject.title == undefined || typeof airplaneObject.title != 'string') airplaneObject.title = 'Airbus A320 Neo Asobo';
-  if (airplaneObject.model == undefined || typeof airplaneObject.model != 'string') airplaneObject.model = '';
-  if (airplaneObject.panel == undefined || typeof airplaneObject.panel != 'string') airplaneObject.panel = '';
-  if (airplaneObject.sound == undefined || typeof airplaneObject.sound != 'string') airplaneObject.sound = '';
-  if (airplaneObject.texture == undefined || typeof airplaneObject.texture != 'string') airplaneObject.texture = '';
-  if (airplaneObject.kb_checklists == undefined || typeof airplaneObject.kb_checklists != 'string')
-    airplaneObject.kb_checklists = 'Boeing747-400_check';
-  if (airplaneObject.kb_reference == undefined || typeof airplaneObject.kb_reference != 'string')
-    airplaneObject.kb_reference = 'Boeing747-400_ref';
-  if (airplaneObject.description == undefined || typeof airplaneObject.description != 'string')
-    airplaneObject.description = 'TT:AIRCRAFT.DESCRIPTION';
-  if (airplaneObject.wip_indicator == undefined || typeof airplaneObject.wip_indicator != 'number') airplaneObject.wip_indicator = 0;
-  if (airplaneObject.ui_manufacturer == undefined || typeof airplaneObject.ui_manufacturer != 'string')
-    airplaneObject.ui_manufacturer = 'TT:AIRCRAFT.UI_MANUFACTURER';
-  if (airplaneObject.ui_type == undefined || typeof airplaneObject.ui_type != 'string') airplaneObject.ui_type = 'A320neo';
-  if (airplaneObject.ui_variation == undefined || typeof airplaneObject.ui_variation != 'string') airplaneObject.ui_variation = 'A320neo';
-  if (airplaneObject.ui_typerole == undefined || typeof airplaneObject.ui_typerole != 'string')
-    airplaneObject.ui_typerole = 'Commercial Airliner';
-  if (airplaneObject.ui_createdby == undefined || typeof airplaneObject.ui_createdby != 'string') airplaneObject.ui_createdby = 'Asobo Studio';
-  if (airplaneObject.ui_thumbnailfile == undefined || typeof airplaneObject.ui_thumbnailfile != 'string') airplaneObject.ui_thumbnailfile = '';
-  if (airplaneObject.ui_certified_ceiling == undefined || typeof airplaneObject.ui_certified_ceiling != 'number')
-    airplaneObject.ui_certified_ceiling = 39800;
-  if (airplaneObject.ui_max_range == undefined || typeof airplaneObject.ui_max_range != 'number') airplaneObject.ui_max_range = 3500;
-  if (airplaneObject.ui_autonomy == undefined || typeof airplaneObject.ui_autonomy != 'number') airplaneObject.ui_autonomy = 7;
-  if (airplaneObject.ui_fuel_burn_rate == undefined || typeof airplaneObject.ui_fuel_burn_rate != 'number')
-    airplaneObject.ui_fuel_burn_rate = 5300;
-  if (airplaneObject.atc_id == undefined || typeof airplaneObject.atc_id != 'string') airplaneObject.atc_id = 'I-ADJU';
-  if (airplaneObject.atc_id_enable == undefined || typeof airplaneObject.atc_id_enable != 'number') airplaneObject.atc_id_enable = 0;
-  if (airplaneObject.atc_airline == undefined || typeof airplaneObject.atc_airline != 'string') airplaneObject.atc_airline = 'DLA';
-  if (airplaneObject.atc_flight_number == undefined || typeof airplaneObject.atc_flight_number != 'string')
-    airplaneObject.atc_flight_number = '1123';
-  if (airplaneObject.atc_heavy == undefined || typeof airplaneObject.atc_heavy != 'number') airplaneObject.atc_heavy = 1;
-  if (airplaneObject.atc_parking_types == undefined || typeof airplaneObject.atc_parking_types != 'string')
-    airplaneObject.atc_heavy = 'GATE,RAMP,CARGO';
-  if (airplaneObject.atc_parking_codes == undefined || typeof airplaneObject.atc_parking_codes != 'string')
-    airplaneObject.atc_parking_codes = '';
-  if (airplaneObject.atc_id_color == undefined || typeof airplaneObject.atc_id_color != 'string') airplaneObject.atc_id_color = '';
-  if (airplaneObject.atc_id_font == undefined || typeof airplaneObject.atc_id_font != 'string') airplaneObject.atc_heavy = '';
-  if (airplaneObject.icao_airline == undefined || typeof airplaneObject.icao_airline != 'string') airplaneObject.atc_heavy = 'DLA';
-  if (airplaneObject.isAirTraffic == undefined || typeof airplaneObject.isAirTraffic != 'number') airplaneObject.isAirTraffic = 0;
-  if (airplaneObject.isUserSelectable == undefined || typeof airplaneObject.isUserSelectable != 'number') airplaneObject.isUserSelectable = 1;
+  if (typeof airplaneObject.title != 'string') airplaneObject.title = 'Airbus A320 Neo Asobo';
+  if (typeof airplaneObject.model != 'string') airplaneObject.model = '';
+  if (typeof airplaneObject.panel != 'string') airplaneObject.panel = '';
+  if (typeof airplaneObject.sound != 'string') airplaneObject.sound = '';
+  if (typeof airplaneObject.texture != 'string') airplaneObject.texture = '';
+  if (typeof airplaneObject.kb_checklists != 'string') airplaneObject.kb_checklists = 'Boeing747-400_check';
+  if (typeof airplaneObject.kb_reference != 'string') airplaneObject.kb_reference = 'Boeing747-400_ref';
+  if (typeof airplaneObject.description != 'string') airplaneObject.description = 'TT:AIRCRAFT.DESCRIPTION';
+  if (typeof airplaneObject.wip_indicator != 'number') airplaneObject.wip_indicator = 0;
+  if (typeof airplaneObject.ui_manufacturer != 'string') airplaneObject.ui_manufacturer = 'TT:AIRCRAFT.UI_MANUFACTURER';
+  if (typeof airplaneObject.ui_type != 'string') airplaneObject.ui_type = 'A320neo';
+  if (typeof airplaneObject.ui_variation != 'string') airplaneObject.ui_variation = 'A320neo';
+  if (typeof airplaneObject.ui_typerole != 'string') airplaneObject.ui_typerole = 'Commercial Airliner';
+  if (typeof airplaneObject.ui_createdby != 'string') airplaneObject.ui_createdby = 'Asobo Studio';
+  if (typeof airplaneObject.ui_thumbnailfile != 'string') airplaneObject.ui_thumbnailfile = '';
+  if (typeof airplaneObject.ui_certified_ceiling != 'number') airplaneObject.ui_certified_ceiling = 39800;
+  if (typeof airplaneObject.ui_max_range != 'number') airplaneObject.ui_max_range = 3500;
+  if (typeof airplaneObject.ui_autonomy != 'number') airplaneObject.ui_autonomy = 7;
+  if (typeof airplaneObject.ui_fuel_burn_rate != 'number') airplaneObject.ui_fuel_burn_rate = 5300;
+  if (typeof airplaneObject.atc_id != 'string') airplaneObject.atc_id = 'I-ADJU';
+  if (typeof airplaneObject.atc_id_enable != 'number') airplaneObject.atc_id_enable = 0;
+  if (typeof airplaneObject.atc_airline != 'string') airplaneObject.atc_airline = 'DLA';
+  if (typeof airplaneObject.atc_flight_number != 'string') airplaneObject.atc_flight_number = '1123';
+  if (typeof airplaneObject.atc_heavy != 'number') airplaneObject.atc_heavy = 1;
+  if (typeof airplaneObject.atc_parking_types != 'string') airplaneObject.atc_heavy = 'GATE,RAMP,CARGO';
+  if (typeof airplaneObject.atc_parking_codes != 'string') airplaneObject.atc_parking_codes = '';
+  if (typeof airplaneObject.atc_id_color != 'string') airplaneObject.atc_id_color = '';
+  if (typeof airplaneObject.atc_id_font != 'string') airplaneObject.atc_heavy = '';
+  if (typeof airplaneObject.icao_airline != 'string') airplaneObject.atc_heavy = 'DLA';
+  if (typeof airplaneObject.isAirTraffic != 'number') airplaneObject.isAirTraffic = 0;
+  if (typeof airplaneObject.isUserSelectable != 'number') airplaneObject.isUserSelectable = 1;
 
   return airplaneObject;
 }
