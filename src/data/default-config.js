98243021e1ca58b2cb4c5641b754c0e1d61c5523
David Wheatley <hi@davwheat.dev>
2020-08-27 17:35:46 +0100

    Add default config

---
+++ src/data/default-config.js
@@ -0,0 +1,7 @@
+import CONFIG_KEYS from './config-keys.json';
+
+export default {
+  [CONFIG_KEYS.state.setup_completed]: false,
+  [CONFIG_KEYS.settings.package_directory]: null,
+  [CONFIG_KEYS.settings.third_party_sources]: [],
+};
