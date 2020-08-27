69ff8bcafc8904b684af27a1f46ab6da591b850e
Ninja <fraser@ninjalabs.co.uk>
2020-08-27 03:55:15 +0100

    Inital settings page

--- src/components/HomeTabs/Settings.js
+++ src/components/HomeTabs/Settings.js
@@ -1,11 +1,117 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Typography } from '@material-ui/core';
+import { 
+  Paper, 
+  Typography, 
+  TextField,
+  IconButton,
+  InputAdornment,
+  Box,
+  Button,
+  useTheme,
+  makeStyles
+} from '@material-ui/core';
+
+import { GetPackagesDirectory, ValidateFSDirectory } from '../../helpers/MSFS';
+import Electron from 'electron';
+import FolderSearchOutlineIcon from 'mdi-react/FolderSearchOutlineIcon';
+import Config from 'electron-json-config';
+import ConfigKeys from '../../data/config-keys.json';
+
+const useStyles = makeStyles({
+  settingsRoot: {
+    flexGrow: 1,
+    padding: 28
+  },
+  pageDescription: {
+    fontSize: 14,
+    opacity: .75
+  }
+})
 
 export default function Settings() {
+  const theme = useTheme();
+  const classes = useStyles();
+  const [error, setError] = useState(null);
+  const [data, setDataReal] = useState({ packageDir: undefined, SaveButtonEnabled: false });
+
+  function setData(d) {
+    setDataReal({ ...data, ...d });
+  }
+
+  if (typeof data.packageDir === 'undefined') {
+    GetPackagesDirectory().then(p => {
+      setData({ packageDir: p });
+    });
+  }
+
+  function openBrowseDialog() {
+    const d = Electron.remote.dialog.showOpenDialogSync(null, { properties: ['openDirectory'] });
+
+    if (typeof d === 'undefined') {
+      // Dialog cancelled
+      return;
+    }
+
+    setData({ packageDir: d[0] });
+
+    const [isValid, errorMsg] = ValidateFSDirectory(d[0]);
+
+    if (isValid) {
+      setError(null);
+      setSaveButtonEnabled(true);
+    } else {
+      setError(errorMsg);
+      setSaveButtonEnabled(false);
+    }
+  }
+
+  function setSaveButtonEnabled(value) {
+    console.log(data.SaveButtonEnabled)
+    data.SaveButtonEnabled = value
+    console.log(data.SaveButtonEnabled)
+  }
+
   return (
-    <div>
-      <Typography paragraph>This is the Settings page.</Typography>
-    </div>
+    <>
+      <Paper className={classes.settingsRoot}>
+        <Typography paragraph className={classes.pageDescription}>Change Simulator Package Directory</Typography>
+        <TextField
+        error={!!error}
+        helperText={error}
+        InputLabelProps={{ shrink: true }}
+        margin="normal"
+        InputProps={{
+          style: { fontFamily: 'IBM Plex Mono', letterSpacing: -0.2 },
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton aria-label="toggle password visibility" onClick={openBrowseDialog}>
+                <FolderSearchOutlineIcon />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+        variant="filled"
+        label="Install path"
+        value={data.packageDir || ''}
+        onChange={e => {
+          setData({ packageDir: e.target.value });
+        }}
+        fullWidth
+      />
+
+        <Box display="flex" padding={theme.spacing()} paddingBottom={theme.spacing(0.5)}>
+          <Box flex="1" />
+          <Button
+            onClick={() => {
+              console.log("saved")
+            }}
+            disabled={data.SaveButtonEnabled}
+          >
+            Save
+          </Button>
+        </Box>
+      </Paper>
+    </>
   );
 }
