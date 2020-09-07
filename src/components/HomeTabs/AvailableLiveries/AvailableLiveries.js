afdf53524faa4c7c9ca84fdb6281c7eda9a096fd
David Wheatley <hi@davwheat.dev>
2020-09-07 18:41:56 +0100

    Expose AvailableLiveries from HomeTabs

--- src/components/HomeTabs/AvailableLiveries/AvailableLiveries.js
+++ src/components/HomeTabs/AvailableLiveries/AvailableLiveries.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Typography } from '@material-ui/core';
 
 export default function AvailableLiveries() {
+  const [availableLiveries, setAvailableLiveries] = useState(null);
+
   return (
     <div>
       <Typography paragraph>This is the Available Liveries page.</Typography>
