ec36e992ade9e59b3d1a2566783eb5f34cb4bd4d
David Wheatley <hi@davwheat.dev>
2020-09-07 18:41:12 +0100

    Only expose functions in dev mode

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import { render } from 'react-dom';
 import App from './components/App';
 
 import '@babel/polyfill';
+import 'overlayscrollbars/css/OverlayScrollbars.css';
 
 import Navigate from './helpers/Navigate';
 import Routes from './data/Routes';
 import Config from 'electron-json-config';
-
-import 'overlayscrollbars/css/OverlayScrollbars.css';
+import IsDev from './data/IsDev';
+import { GetSourceList } from './helpers/Manifest';
 
 // Since we are using HtmlWebpackPlugin WITHOUT a template, we should create our own root node in the body element before rendering into it
 let root = document.createElement('div');
@@ -19,11 +20,15 @@ document.body.appendChild(root);
 // Now we can render our application into it
 render(<App />, document.getElementById('root'));
 
-// if (process.)
-InitialiseDeveloperFunctions();
+if (IsDev) InitialiseDeveloperFunctions();
 
 function InitialiseDeveloperFunctions() {
   window.__navigate = Navigate;
   window.__routes = Routes;
   window.__config = Config;
+  window.__helpers = {
+    manifest: {
+      GetSourceList: GetSourceList,
+    },
+  };
 }
