923aacb67b4c22c668f2e9e1e2a51e3b4d42233e
David Wheatley <hi@davwheat.dev>
2020-09-12 01:51:08 +0100

    Add option to force production API

--- src/data/ActiveApiEndpoint.js
+++ src/data/ActiveApiEndpoint.js
@@ -1,6 +1,6 @@
 import Constants from './Constants.json';
 import IsDev from './IsDev';
 
-const ActiveApiEndpoint = IsDev ? Constants.urls.devApiEndpoint : Constants.urls.apiEndpoint;
+const ActiveApiEndpoint = IsDev && !Constants.api.forceProdApi ? Constants.urls.devApiEndpoint : Constants.urls.apiEndpoint;
 
 export default ActiveApiEndpoint;
