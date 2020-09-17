5d56ea02e4be4eb7214b7343fd3e43918a8ba04f
David Wheatley <hi@davwheat.dev>
2020-09-17 22:37:59 +0100

    Wrap installation function in promise

--- src/helpers/AddonInstaller/InstallAddon.js
+++ src/helpers/AddonInstaller/InstallAddon.js
@@ -22,37 +22,41 @@ export default async function InstallAddon(PlaneObject) {
   const downloadURL = `${Constants.urls.cdnEndpoint}/${PlaneObject.fileName}?hash=${PlaneObject.checkSum}`;
   const zipName = PlaneObject.fileName.substr(PlaneObject.fileName.lastIndexOf('/') + 1);
   const tempPath = Path.join(remote.app.getPath('temp'), Constants.appName, Constants.dirs.downloadCache);
+  const extractDir = Path.join(Directory, `Community\\${zipName.split('.zip')[0]}`);
 
-  request
-    .get(downloadURL)
-    .on('error', function (error) {
-      ThrowError('E008', `${error} URL: ${downloadURL}`);
-    })
-    .pipe(fs.createWriteStream(downloadDir))
-    .on('finish', async function () {
-      console.log(`Finished downloading: ${zipName}`);
-      const zip = new admzip(downloadDir);
-      const extractDir = Path.join(Directory, `Community\\${zipName.split('.zip')[0]}`);
-      fs.mkdir(
-        extractDir,
-        err =>
-          async function () {
-            if (err) {
-              fs.unlinkSync(downloadDir);
-              return console.error(err);
+  await new Promise((resolve, reject) => {
+    request
+      .get(downloadURL)
+      .on('error', function (err) {
+        ThrowError('E008', `${err} URL: ${downloadURL}`);
+        reject(err);
+      })
+      .pipe(fs.createWriteStream(tempPath))
+      .on('finish', async function () {
+        console.log(`Finished downloading: ${zipName}`);
+        const zip = new admzip(tempPath);
+        fs.mkdir(
+          extractDir,
+          err =>
+            async function () {
+              if (err) {
+                fs.unlinkSync(tempPath);
+                reject(err);
+              }
             }
+        );
+        console.log(`Created folder \n${extractDir}`);
+        zip.extractAllToAsync(`${extractDir}`, /*overwrite*/ true, function () {
+          try {
+            fs.unlinkSync(tempPath);
+            fs.writeFileSync(Path.join(extractDir, `do-not-touch.json`), JSON.stringify(PlaneObject, null, 2));
+            console.log(`Installed: ${zipName}`);
+            resolve();
+          } catch (err) {
+            fs.unlinkSync(tempPath);
+            reject(err);
           }
-      );
-      console.log(`Created folder \n${extractDir}`);
-      zip.extractAllToAsync(`${extractDir}`, /*overwrite*/ true, function () {
-        try {
-          fs.unlinkSync(downloadDir);
-          fs.writeFileSync(Path.join(extractDir, `do-not-touch.json`), JSON.stringify(PlaneObject, null, 2));
-          console.log(`Installed: ${zipName}`);
-        } catch (error) {
-          console.log(error);
-          fs.unlinkSync(downloadDir);
-        }
+        });
       });
-    });
+  });
 }
