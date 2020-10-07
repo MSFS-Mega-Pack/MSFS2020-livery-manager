56b5f337e7a68e4530e447637bbac4ea7f22d8d8
David Wheatley <hi@davwheat.dev>
2020-10-07 02:27:59 +0100

    Add advanced settings config entries

--- src/data/default-config.js
+++ src/data/default-config.js
@@ -4,4 +4,5 @@ export default {
   [CONFIG_KEYS.state.setup_completed]: false,
   [CONFIG_KEYS.settings.package_directory]: null,
   [CONFIG_KEYS.settings.third_party_sources]: [],
+  [CONFIG_KEYS.settings.show_advanced_settings]: false,
 };
