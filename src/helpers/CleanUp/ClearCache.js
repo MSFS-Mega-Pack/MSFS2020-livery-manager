3f343a17e77d7304478c0c980bd6c4500a91b629
David Wheatley <hi@davwheat.dev>
2020-10-07 14:35:08 +0100

    Update Clear Cache helper

--- src/helpers/CleanUp/ClearCache.js
+++ src/helpers/CleanUp/ClearCache.js
@@ -1,12 +1,14 @@
 import { remote } from 'electron';
 import Constants from '../../data/Constants.json';
 import Path from 'path';
-import { promises as fs } from 'fs';
+import fs from 'fs';
 
 export default async function ClearCache() {
   const tempPath = Path.join(remote.app.getPath('temp'), Constants.appName, Constants.dirs.downloadCache);
 
-  await fs.rmdir(tempPath, { recursive: true });
+  if (fs.existsSync(tempPath)) {
+    await fs.promises.rmdir(tempPath, { recursive: true });
+  }
 
   return true;
 }
