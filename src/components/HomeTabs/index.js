16ee7d2dd6e23b27a38f19da316bc28ef9a84c11
David Wheatley <hi@davwheat.dev>
2020-10-07 14:34:56 +0100

    Move InstalledLiveries tab content to subdirectory

--- src/components/HomeTabs/index.js
+++ src/components/HomeTabs/index.js
@@ -1,4 +1,4 @@
 export { default as AvailableLiveries } from './AvailableLiveries/AvailableLiveries';
-export { default as InstalledLiveries } from './InstalledLiveries';
+export { default as InstalledLiveries } from './InstalledLiveries/InstalledLiveries';
 export { default as Feed } from './Feed/Feed';
 export { default as Settings } from './Settings';
