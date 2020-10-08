c82c0ce5e9d49303b1d7ded10824e40216dd8af4
David Wheatley <hi@davwheat.dev>
2020-10-08 17:39:38 +0100

    Return false if cache clear fails

--- src/helpers/CleanUp/ClearCache.js
+++ src/helpers/CleanUp/ClearCache.js
@@ -4,10 +4,14 @@ import Path from 'path';
 import fs from 'fs';
 
 export default async function ClearCache() {
-  const tempPath = Path.join(remote.app.getPath('temp'), Constants.appName, Constants.dirs.downloadCache);
+  try {
+    const tempPath = Path.join(remote.app.getPath('temp'), Constants.appName, Constants.dirs.downloadCache);
 
-  if (fs.existsSync(tempPath)) {
-    await fs.promises.rmdir(tempPath, { recursive: true });
+    if (fs.existsSync(tempPath)) {
+      await fs.promises.rmdir(tempPath, { recursive: true });
+    }
+  } catch {
+    return false;
   }
 
   return true;
