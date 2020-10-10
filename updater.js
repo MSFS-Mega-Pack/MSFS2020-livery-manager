4caae36c17611da7eaed09d00e3f744e4fd81c41
David Wheatley <hi@davwheat.dev>
2020-10-10 15:39:02 +0100

    Add auto-update

--- updater.js
+++ updater.js
@@ -1,43 +1,69 @@
+const { ipcMain } = require('electron');
 const electron = require('electron');
+const fs = require('fs');
+const { default: fetch } = require('node-fetch');
+const open = require('open');
+const Path = require('path');
+const request = require('request');
+const reqProgress = require('request-progress');
 
 // The current version of your app.
 const APP_VERSION = require('./package.json').version;
+const APP_NAME = require('./package.json').name;
+const UPDATE_URL = `https://msfs-liverypack-api.mrproper.dev/v1/get/update`;
 
-// The url that the application is going to query for new release
-const AUTO_UPDATE_URL =
-  'https://api.update.rocks/update/github.com/MSFS-Mega-Pack/MSFS2020-livery-manager/stable/' + process.platform + '/' + APP_VERSION;
+async function checkForUpdates(window) {
+  let json;
 
-function checkForUpdates(window) {
-  if (process.platform === 'linux') {
-    /* There is no auto update for linux however you can still
-       notify the user that a new update has been released
-       our service will return an answer with the latest version. */
-    console.log('Auto updates not available on linux');
-  } else {
-    initDarwinWin32(window);
+  try {
+    json = await (await fetch(`${UPDATE_URL}/${APP_VERSION}`)).json();
+  } catch {
+    // broken api??
+    return;
   }
-}
 
-function initDarwinWin32(window) {
-  electron.autoUpdater.on('error', err => console.error(`Update error: ${err.message}`));
-
-  electron.autoUpdater.on('checking-for-update', () => console.log('Checking for update'));
-
-  electron.autoUpdater.on('update-available', () => {
-    console.log('Update available');
-
+  if (json.data.update) {
+    // Update is available!
     window.loadURL(`file://${__dirname}/update-available.html`);
-  });
 
-  electron.autoUpdater.on('update-not-available', () => console.log('No update available'));
+    const updateDir = Path.join(electron.app.getPath('temp'), APP_NAME, `/updates`);
+    const exePath = Path.join(updateDir, `/setup_${json.data.info.latest}.exe`);
 
-  // Ask the user if he wants to update if update is available
-  electron.autoUpdater.on('update-downloaded', () => {
-    electron.autoUpdater.quitAndInstall();
-  });
+    if (fs.existsSync(exePath)) {
+      await fs.promises.unlink(exePath);
+    }
 
-  electron.autoUpdater.setFeedURL(AUTO_UPDATE_URL);
-  electron.autoUpdater.checkForUpdates();
+    if (!fs.existsSync(updateDir)) {
+      await fs.promises.mkdir(updateDir, { recursive: true });
+    }
+
+    // let progress = null;
+
+    // ipcMain.on('get-progress', e => {
+    //   e.reply('download-progress', progress);
+    // });
+
+    await new Promise((resolve, reject) => {
+      // reqProgress(
+      request(json.data.info.downloadUrl)
+        // ,
+        //   { throttle: 100 }
+        // )
+        .pipe(fs.createWriteStream(exePath))
+        // .on('progress', p => {
+        //   progress = p;
+        // })
+        .on('error', async err => {
+          console.log(`Error`);
+          console.error(err);
+          reject(err);
+        })
+        .on('finish', async () => {
+          open(exePath);
+          resolve();
+        });
+    });
+  }
 }
 
 module.exports = {
