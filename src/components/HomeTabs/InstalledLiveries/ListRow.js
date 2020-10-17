37f00a2b4c61ae33a13e609837dd6e595194735a
davwheat <hi@davwheat.dev>
2020-10-17 12:41:16 +0100

    Commit WIP code

--- src/components/HomeTabs/InstalledLiveries/ListRow.js
+++ src/components/HomeTabs/InstalledLiveries/ListRow.js
@@ -1,36 +1,82 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { IconButton, ListItem, ListItemText, Tooltip } from '@material-ui/core';
 import BinIcon from 'mdi-react/TrashCanOutlineIcon';
 import UpdateIcon from 'mdi-react/DownloadOutlineIcon';
+import { useSnackbar } from 'notistack';
 
 export default function ListRow(props) {
   const { livery, updateAvailable } = props;
+
+  const removeButton = useRef(null);
+  const [deleteTooltipText, setDeleteTooltipText] = useState('Remove');
+  const [isDisabled, setisDisabled] = useState(false);
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  useEffect(() => {
+    let timeout,
+      interval,
+      timeRemaining = 2.0;
+    let startedDeleting = false;
+
+    function RemoveBtnMouseDown() {
+      timeout = setTimeout(() => {
+        if (!startedDeleting) {
+          startedDeleting = true;
+          alert('2s');
+          startedDeleting = false;
+        }
+      }, 2000);
+
+      interval = setInterval(() => {
+        timeRemaining -= 0.1;
+        if (timeRemaining < 0) {
+          timeRemaining = 0;
+          clearInterval(interval);
+          return;
+        }
+
+        console.log('update text');
+        setDeleteTooltipText(`Hold for ${timeRemaining.toFixed(1)}s`);
+      }, 100);
+    }
+
+    function RemoveBtnMouseUp() {
+      clearTimeout(timeout);
+      clearInterval(interval);
+    }
+    function RemoveBtnClick() {
+      enqueueSnackbar('To remove a livery, click and hold the button');
+    }
+
+    if (removeButton && removeButton.current) {
+      removeButton.current.addEventListener('mousedown', RemoveBtnMouseDown);
+      removeButton.current.addEventListener('mouseup', RemoveBtnMouseUp);
+    }
+
+    return function () {
+      removeButton.current.removeEventListener('mousedown', RemoveBtnMouseDown);
+      removeButton.current.removeEventListener('mouseup', RemoveBtnMouseUp);
+    };
+  });
+
+  console.log(deleteTooltipText);
 
   return (
     <>
       <ListItem>
         <ListItemText primary={livery.fileName.substr(livery.fileName.indexOf('/') + 1).split('.zip')[0]} />
         {updateAvailable && (
-          <Tooltip title={'Update'}>
-            <IconButton
-              color="primary"
-              onClick={e => {
-                e.stopPropagation();
-              }}
-            >
+          <Tooltip title={deleteTooltipText}>
+            <IconButton color="primary">
               <UpdateIcon />
             </IconButton>
           </Tooltip>
         )}
         <Tooltip title={'Remove'}>
-          <IconButton
-            color="primary"
-            onClick={e => {
-              e.stopPropagation();
-            }}
-          >
+          <IconButton ref={removeButton} color="primary">
             <BinIcon />
           </IconButton>
         </Tooltip>
