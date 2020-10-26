99a90254565ec7abf7b8480580a6fcef535cf03b
David Wheatley <hi@davwheat.dev>
2020-10-26 16:19:10 +0100

    Fix FD error (hopefully!) (Fixes #138)

--- src/pages/Setup.js
+++ src/pages/Setup.js
@@ -13,15 +13,21 @@ import Electron from 'electron';
 import Config from 'electron-json-config';
 import ConfigKeys from '../data/config-keys.json';
 import PackageJson from '../../package.json';
+import LocaleContext from '../locales/LocaleContext';
 
 export default function Setup() {
   const [page, setPage] = useState(1);
   const [nextButtonEnabled, setNextButtonEnabled] = useState(true);
   const [data, setDataReal] = useState({ packageDir: undefined });
 
+  const theme = useTheme();
+  const CurrentLocale = React.useContext(LocaleContext);
+
   if (page === 2) {
-    if (!data.packageDir || data.packageDir.trim() === '') {
+    if (!ValidateFSDirectory(data.packageDir, CurrentLocale)[0]) {
       nextButtonEnabled !== false && setNextButtonEnabled(false);
+    } else {
+      nextButtonEnabled !== true && setNextButtonEnabled(true);
     }
   }
 
@@ -34,8 +40,6 @@ export default function Setup() {
       setData({ packageDir: p });
     });
   }
-
-  const theme = useTheme();
 
   let CurrentPage = Pages[page - 1] || <></>;
 
@@ -53,7 +57,7 @@ export default function Setup() {
             }}
             disabled={page === 1}
           >
-            Back
+            {CurrentLocale.translate('manager.setup.back_button')}
           </Button>
         }
         <Box flex="1" />
@@ -71,7 +75,7 @@ export default function Setup() {
           }}
           disabled={!nextButtonEnabled}
         >
-          {page === Pages.length ? 'Finish' : 'Next'}
+          {page === Pages.length ? CurrentLocale.translate('manager.setup.finish_button') : CurrentLocale.translate('manager.setup.next_button')}
         </Button>
       </Box>
     </Box>
@@ -81,20 +85,23 @@ export default function Setup() {
 const Pages = [WelcomePage, SimInstallDirectoryPage, SetupCompletePage];
 
 function WelcomePage() {
+  const CurrentLocale = React.useContext(LocaleContext);
+
   return (
     <>
       <Typography gutterBottom component="h1" variant="h4">
-        Welcome to the {PackageJson.productName}
+        {CurrentLocale.translate('manager.setup.welcome_page.heading', { app_name: PackageJson.productName })}
       </Typography>
       <Typography gutterBottom component="p" variant="body1">
-        We need to go through a few brief setup pages before you can use the app, just to give you the best experience.
+        {CurrentLocale.translate('manager.setup.welcome_page.description')}
       </Typography>
     </>
   );
 }
 
-function SimInstallDirectoryPage({ data, setData, setNextButtonEnabled }) {
-  const [error, setError] = useState(null);
+function SimInstallDirectoryPage({ data, setData }) {
+  const CurrentLocale = React.useContext(LocaleContext);
+  const errorMsg = ValidateFSDirectory(data.packageDir, CurrentLocale)[1];
 
   function openBrowseDialog() {
     const d = Electron.remote.dialog.showOpenDialogSync(null, { properties: ['openDirectory'] });
@@ -105,44 +112,36 @@ function SimInstallDirectoryPage({ data, setData, setNextButtonEnabled }) {
     }
 
     setData({ packageDir: d[0] });
-
-    const [isValid, errorMsg] = ValidateFSDirectory(d[0]);
-
-    if (isValid) {
-      setError(null);
-      setNextButtonEnabled(true);
-    } else {
-      setError(errorMsg);
-      setNextButtonEnabled(false);
-    }
   }
 
   return (
     <>
       <Typography gutterBottom component="h1" variant="h4">
-        Simulator packages directory
+        {CurrentLocale.translate('manager.setup.community_directory.heading')}
       </Typography>
       <Typography gutterBottom component="p" variant="body1">
-        Please check that the directory below matches your Community folder path. If it doesn&apos;t, choose the correct directory with the
-        browse button.
+        {CurrentLocale.translate('manager.setup.community_directory.description')}
       </Typography>
       <TextField
-        error={!!error}
-        helperText={error}
+        error={!!errorMsg}
+        helperText={errorMsg}
         InputLabelProps={{ shrink: true }}
         margin="normal"
         InputProps={{
           style: { fontFamily: 'IBM Plex Mono', letterSpacing: -0.2 },
           endAdornment: (
             <InputAdornment position="end">
-              <IconButton aria-label="toggle password visibility" onClick={openBrowseDialog}>
+              <IconButton
+                aria-label={CurrentLocale.translate('manager.setup.community_directory.text_field.browse_button_sr_text')}
+                onClick={openBrowseDialog}
+              >
                 <FolderSearchOutlineIcon />
               </IconButton>
             </InputAdornment>
           ),
         }}
         variant="filled"
-        label="Install path"
+        label={CurrentLocale.translate('manager.setup.community_directory.text_field.label')}
         value={data.packageDir || ''}
         onChange={e => {
           setData({ packageDir: e.target.value });
@@ -154,13 +153,18 @@ function SimInstallDirectoryPage({ data, setData, setNextButtonEnabled }) {
 }
 
 function SetupCompletePage() {
+  const CurrentLocale = React.useContext(LocaleContext);
+
   return (
     <>
       <Typography gutterBottom component="h1" variant="h4">
-        Setup complete
+        {CurrentLocale.translate('manager.setup.complete_page.heading')}
       </Typography>
       <Typography gutterBottom component="p" variant="body1">
-        You&apos;re ready to take off!
+        {CurrentLocale.translate('manager.setup.complete_page.description1')}
+      </Typography>
+      <Typography gutterBottom component="p" variant="body1">
+        {CurrentLocale.translate('manager.setup.complete_page.description2')}
       </Typography>
     </>
   );
