601aee4ec66813c394725b860d4c6e41a12859aa
David Wheatley <hi@davwheat.dev>
2020-10-28 14:22:36 +0100

    Add more strings

--- src/components/RefreshBox.js
+++ src/components/RefreshBox.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { Box, IconButton, makeStyles, Paper, Tooltip, Typography } from '@material-ui/core';
 import RefreshIcon from 'mdi-react/RefreshIcon';
 import dayjs from 'dayjs';
+import LocaleContext from '../locales/LocaleContext';
 
 const useStyles = makeStyles({
   root: { marginBottom: 16, marginTop: -8 },
@@ -11,27 +12,32 @@ const useStyles = makeStyles({
 });
 
 export default function RefreshBox(props) {
-  const { lastCheckedTime, justRefreshed, onRefresh, disabled, refreshInterval } = props;
+  const { lastCheckedTime, justRefreshed, onRefresh, refreshInterval } = props;
   const classes = useStyles();
+  const CurrentLocale = React.useContext(LocaleContext);
 
-  let toolTipContent = 'Refresh available liveries';
-  if (justRefreshed) toolTipContent = `Rate limiting is in effect: you need to wait at least ${refreshInterval / 1000}s between refreshes`;
-  else if (disabled) toolTipContent = `You need to wait until your liveries are installed before refreshing.`;
+  let toolTipContent = CurrentLocale.translate('manager.components.refresh_box.refresh_button.tooltip.default');
+  if (justRefreshed)
+    toolTipContent = CurrentLocale.translate('manager.components.refresh_box.refresh_button.tooltip.default', {
+      interval: refreshInterval / 1000,
+    });
 
   return (
     <Paper className={classes.root} variant="outlined">
       <Box px={2} py={1} display="flex" flexDirection="row">
         <Typography color="textSecondary" variant="body2" className={classes.text}>
           <Tooltip title="When you last checked for installed and available liveries">
-            <span>Last refreshed:</span>
+            <span>{CurrentLocale.translate('manager.components.refresh_box.refresh_button.last_refreshed')}</span>
           </Tooltip>{' '}
-          {typeof lastCheckedTime === 'string' ? lastCheckedTime : dayjs(lastCheckedTime).format('D MMM YYYY, h:mm A') || 'unknown'}
+          {typeof lastCheckedTime === 'string'
+            ? lastCheckedTime
+            : dayjs(lastCheckedTime).format('D MMM YYYY, h:mm A') || CurrentLocale.translate('manager.components.refresh_box.unknown_time')}
         </Typography>
         <Box flex={1} />
         <Box>
           <Tooltip title={toolTipContent}>
             <span>
-              <IconButton color="primary" size="small" disabled={!!justRefreshed || disabled} onClick={onRefresh}>
+              <IconButton color="primary" size="small" disabled={!!justRefreshed} onClick={onRefresh}>
                 <RefreshIcon />
               </IconButton>
             </span>
@@ -46,6 +52,5 @@ RefreshBox.propTypes = {
   lastCheckedTime: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
   justRefreshed: PropTypes.bool,
   onRefresh: PropTypes.func,
-  disabled: PropTypes.bool,
   refreshInterval: PropTypes.number.isRequired,
 };
