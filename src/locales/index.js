294eacc3de98f32b6d1afbe662b4342845d9ba51
Breze <39080460+brezedc@users.noreply.github.com>
2020-11-17 10:52:27 +0100

    added swedish

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,2 +1,6 @@
-export { default as enGB } from './en-GB';
-export { default as enUS } from './en-US';
+export { default as enGB }
+from './en-GB';
+export { default as enUS }
+from './en-US';
+export { default as seSE }
+from './se-SE';
