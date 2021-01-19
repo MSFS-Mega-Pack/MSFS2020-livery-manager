5ebac882e7c2309888e2949335304d7498563943
Jaap <jaap@mrproper.dev>
2021-01-19 15:22:16 +0100

    Add catch to api call

--- src/helpers/AddonInstaller/addLiveryInstallAnalytic.js
+++ src/helpers/AddonInstaller/addLiveryInstallAnalytic.js
@@ -5,7 +5,9 @@ import ActiveApiEndpoint from '../../data/ActiveApiEndpoint';
 export default async function AddLiveryInstallAnalytic(liveryName) {
   console.log('Reporting livery install to analytics API:', liveryName);
   try {
-    fetch(`${ActiveApiEndpoint}/${Constants.api.get.addLiveryStat}/${liveryName}`);
+    fetch(`${ActiveApiEndpoint}/${Constants.api.get.addLiveryStat}/${liveryName}`).catch(err => {
+      console.log(err);
+    });
   } catch (e) {
     console.log(e);
   }
