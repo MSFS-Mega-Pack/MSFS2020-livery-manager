87f6ae01ba763c23df07d0bd01c0c5fbaff3a141
David Wheatley <hi@davwheat.dev>
2021-02-12 23:30:18 +0000

    Fromat; correct folder name

---
+++ src/locales/zh-Hant/index.js
@@ -0,0 +1,2 @@
+export { default as dayJsLocale } from 'dayjs/locale/zh-hk';
+export default from './locale.json';
--- src/locales/zh_Hant/index.js
+++
@@ -1,2 +0,0 @@
-export { default as dayJsLocale } from 'dayjs/locale/zh-hk';
-export default from './locale.json';
